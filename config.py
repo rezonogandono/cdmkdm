8583b5e38c367570929876463e66e23270f96daa
rezonogandono <doommaster67rus@gmail.com>
2026-08-20 18:44:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ LOG_CHAT_ID = int(os.getenv("LOG_CHAT_ID"))
 BOT_USERNAME = os.getenv("BOT_USERNAME")
 
 # ── Ссылки ──────────────────────────────────────────────────────────────────
-URL_CHANNEL    = "https://"
+URL_CHANNEL    = "https://t.me/tonprice"
 URL_SUPPORT    = os.getenv("URL_SUPPORT")
-URL_MINI_APP   = "https://"
+URL_MINI_APP   = "https://t.me/tonprice"
 
 # ── Пути к изображениям по языкам ───────────────────────────────────────────────
 # Если изображение для конкретного языка отсутствует, используется значение по умолчанию (оригинальное .jpg)
